a5a2bcb0edb76d2008a0f46c3d60b0ec17f0a4d9
Aydyn Tairov <den.tairov@gmail.com>
2022-12-16 23:28:56 +0000

    asdf

--- rendezvouz_hash.py
+++ rendezvouz_hash.py
@@ -18,3 +18,6 @@ nodes = ['node1', 'node2', 'node3']
 key = 'some_key'
 selected_node = rendezvous_hash(key, nodes)
 print(selected_node)  # Outputs one of the nodes in the list
+
+###
+# This implementation uses the hashlib module to calculate a hash value for the key and each node, and then selects the node with the highest hash value. This ensures that keys are distributed evenly across the nodes in the cluster, with a minimal number of remapping operations when nodes are added or removed.
